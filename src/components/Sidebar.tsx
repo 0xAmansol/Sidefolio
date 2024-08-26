82db7c642fe04c833ccae56070d88143b4a5dcba
Aman Kalal <84617672+thecrazyengineerguy@users.noreply.github.com>
2024-08-26 21:56:27 +0530

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -106,15 +106,15 @@ const SidebarHeader = () => {
   return (
     <div className="flex space-x-2">
       <Image
-        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80"
+        src="https://media.licdn.com/dms/image/v2/D5603AQFgcWJLPVFTug/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1710241905452?e=1730332800&v=beta&t=WW6i_7trWNWUCZUS5M5-e9NZhZaAw3g35_tDtFUKHCo"
         alt="Avatar"
         height="40"
         width="40"
         className="object-cover object-top rounded-full flex-shrink-0"
       />
       <div className="flex text-sm flex-col">
-        <p className="font-bold text-primary">John Doe</p>
-        <p className="font-light text-secondary">Developer</p>
+        <p className="font-bold text-primary">Aman Kalal</p>
+        <p className="font-light text-secondary">Full-Stack Developer</p>
       </div>
     </div>
   );
