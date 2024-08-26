c5cd04c4903bf24f38c718cfa77b035112378741
Aman Kalal <84617672+thecrazyengineerguy@users.noreply.github.com>
2024-08-26 12:39:51 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,14 @@ export default function Home() {
   return (
     <Container>
       <span className="text-4xl">👋</span>
-      <Heading className="font-black">Hello there! I&apos;m John</Heading>
+      <Heading className="font-black">Hello there! I&apos;m Aman</Heading>
       <Paragraph className="max-w-xl mt-4">
         I&apos;m a full-stack developer that loves{" "}
-        <Highlight>building products</Highlight> and web apps that can impact
-        millions of lives
+        <Highlight>building products</Highlight> and web apps that can impact.
       </Paragraph>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a senior software engineer with{" "}
-        <Highlight>7 years of experience</Highlight> building scalable web apps
+        I&apos;m a software engineer with{" "}
+        <Highlight>3 years of experience</Highlight> building scalable web apps
         that are performance optimized and good looking.
       </Paragraph>
       <Heading
